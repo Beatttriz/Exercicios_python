f3afe64ebb63ac4dab5fb56181b723bebe777163
Beatriz <araujobeatrizhonorio@gmail.com>
2023-10-23 07:18:25 -0300

    estoy loco lalalalalalala
    aaaaaaaa

---
+++ baixar/projetos-python-txt/aula20.py
@@ -0,0 +1,14 @@
+def lin():
+    print('-'*30)
+def men(msg):
+    print('-'*30)
+    print(msg)
+    print('-'*30)
+
+men('estoy loco lalalal')
+
+lin()
+print('bia')
+lin()
+print('estoy loco')
+lin()
---
+++ baixar/projetos-python-txt/desafio96.py
@@ -0,0 +1,7 @@
+def area(la, co):
+    a=la*co
+    print(f'A área de um terreno {la} x {co} é de {a}m².')
+
+l=float(input('Largura (m): '))
+c=float(input('Comprimento (m): '))
+area(l,c)
